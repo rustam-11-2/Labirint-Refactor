f47cfc06c54f1deec8084939b527d114c6061912
RustamKumykov <rustamkumykov05@gmail.com>
2026-05-29 16:06:36 +0300

    создал новую форму выбора уровня

---
+++ Labirint/NewFormSelectLvl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Labirint
+{
+    public partial class NewFormSelectLvl : Form
+    {
+        public NewFormSelectLvl()
+        {
+            InitializeComponent();
+        }
+
+        private void NewFormSelectLvl_Load(object sender, EventArgs e)
+        {
+            int countLevels = Directory.GetFiles("GameLvls", ".txt").Length; ;
+        }
+    }
+}
